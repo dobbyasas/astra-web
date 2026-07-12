fa46855f176046e9c780d3708707eddaef63be86
dobbyasas <svejkovsky.kry@gmail.com>
2026-07-12 02:10:16 +0200

    Point web API to Fedora server

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -21,7 +21,7 @@ export type PlaybackProgress = {
 }
 
 export const apiBaseUrl =
-  import.meta.env.VITE_API_URL?.replace(/\/$/, '') ?? 'http://192.168.0.16:4000'
+  import.meta.env.VITE_API_URL?.replace(/\/$/, '') ?? 'http://192.168.0.192:4000'
 
 export function getServerMediaUrl(path: string) {
   if (/^https?:\/\//i.test(path)) {
